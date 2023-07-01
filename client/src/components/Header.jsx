8dcaa52f2481a8367fee26a7ae7897b275e495f9
Joshua Ow <joshuaow@gmail.com>
2023-06-30 21:53:55 -0500

    test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,30 +5,18 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [isScrollingUp, setIsScrollingUp] = useState(true);
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
-      setIsScrollingUp(currentScrollTop < lastScrollTop);
+      setIsScrollingDown(currentScrollTop > 50 && currentScrollTop > lastScrollTop);
       setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
     };
 
-    let timeoutId;
+    window.addEventListener('scroll', handleScroll, { passive: true });
 
-    const handleScrollWithDelay = () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        handleScroll();
-      }, 100); // Adjust the delay time as needed
-    };
-
-    window.addEventListener('scroll', handleScrollWithDelay, { passive: true });
-
-    return () => {
-      clearTimeout(timeoutId);
-      window.removeEventListener('scroll', handleScrollWithDelay);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, [lastScrollTop]);
 
   const handleScrollToTop = (event) => {
@@ -42,10 +30,10 @@ function Header() {
   return (
     <header
       style={{
-        transform: isScrollingUp ? 'translateY(0)' : 'translateY(-100%)',
-        transitionDelay: isScrollingUp ? '0s' : '0.2s', // Adjust the delay time as needed
+        transform: isScrollingDown ? 'translateY(-100%)' : 'translateY(0)',
+        transition: 'transform 0.3s ease-out',
       }}
-      className={`fixed top-0 left-50 w-full z-20 bg-background max-w-screen-xl bg-opacity-60 backdrop-blur-md py-3 sm:px-10 sm:pr-8 px-5 flex items-center justify-between transition-all duration-300`}
+      className={`fixed top-0 left-50 w-full z-20 bg-background max-w-screen-xl bg-opacity-60 backdrop-blur-md py-3 sm:px-10 sm:pr-8 px-5 flex items-center justify-between`}
     >
       <nav className='flex justify-between w-full items-center'>
         <a href="#" onClick={handleScrollToTop} className='font-bold text-2xl text-white text-opacity-75'>joshuaow<span className=' text-cyan-600 text-4xl'>.</span></a>
