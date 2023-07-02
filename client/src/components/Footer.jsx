a55959de7e6c1dc29a9bad3df9a72779b2f0a1f5
Joshua Ow <joshuaow@gmail.com>
2023-07-02 14:45:48 -0500

    contrast in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 
     return (
         <footer className="flex justify-center w-full">
-            <span className="text-xs text-white text-opacity-40">&copy; 2023 Joshua Ow. All Rights Reserved. Made with React & Tailwind</span>
+            <span className="text-xs text-white text-opacity-60">&copy; 2023 Joshua Ow. All Rights Reserved. Made with React & Tailwind</span>
         </footer>
     )
 }
